60d7ce427509ccdb929748e2a3badf816fef70af
Val-Osad <v.p.osadchuk@gmail.com>
2022-06-24 15:16:11 +0000

    variables' set

---
+++ js/login.js
@@ -0,0 +1,17 @@
+(function () {
+    const error = document.querySelector(".error");
+    const userName = document.querySelector(".login__username-input");
+    const password = document.querySelector(".login__password-input");
+    const submitBtn = document.querySelector("#submit-btn");
+    
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    //form validation thing
+    //serial: success of failure
+    //message is what to print inside error class
+    let engine = (id, serial, message) => {
+
+    }
+})();
